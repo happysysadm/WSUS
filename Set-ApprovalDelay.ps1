5ee351da0b53603c9ec92587fec292ab63cdcbd6
happysysadm <carlo.le.roi@rocketmail.com>
2018-06-20 11:20:00 +0200

    Add files via upload
    Script used to generate the JSON setting file for Invoke-Wsus.

---
+++ Set-ApprovalDelay.ps1
@@ -0,0 +1,26 @@
+﻿$DelaySettings = @()
+
+$DelaySettings += [pscustomobject]@{
+
+    Name          = 'Immediate'
+
+    WsusGroup   = 'Standard'
+
+    ApprovalDelay = 1
+
+}
+
+$DelaySettings += [pscustomobject]@{
+
+    Name          = 'OneWeek'
+
+    WsusGroup   = 'Touchy','Critical'
+
+    ApprovalDelay = 7
+
+}
+
+
+$DelaySettings | ConvertTo-Json  | Out-File approvaldelaysettings.json -Force
+
+Get-Content approvaldelaysettings.json | ConvertFrom-Json
